24148765a05a5a4e14b74dbff11f6e48e65aabc8
md-abu-kayser <abu.kayser.official@gmail.com>
2026-05-22 18:27:07 +0600

    add request logging middleware with response time tracking

---
+++ src/middlewares/requestLogger.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import { logger } from "@/shared/logger";
+
+export const requestLogger = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const start = Date.now();
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    logger.http(
+      `${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`,
+    );
+  });
+  next();
+};
