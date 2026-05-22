f4025dcc56afd47953abba652c16ae07d726ba78
md-abu-kayser <abu.kayser.official@gmail.com>
2026-05-23 02:02:43 +0600

    modules updated

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,12 +1,12 @@
-import { Router } from "express";
-import userRoutes from "./users/user.routes";
-import productRoutes from "./products/product.routes";
-import orderRoutes from "./orders/order.routes";
+import { Router } from 'express';
+import userRoutes from './users/user.routes';
+import productRoutes from './products/product.routes';
+import orderRoutes from './orders/order.routes';
 
 const router = Router();
 
-router.use("/users", userRoutes);
-router.use("/products", productRoutes);
-router.use("/orders", orderRoutes);
+router.use('/users', userRoutes);
+router.use('/products', productRoutes);
+router.use('/orders', orderRoutes);
 
 export default router;
